2e0beac68d0e32dfc3a0ac7990eb742aa84286d5
Iliyan Kafedzhiev <iliyan.kafedzhiev@mobisystems.com>
2015-06-22 11:33:21 +0300

    Created application with test web view.

--- MobiStore/ViewController.swift
+++ MobiStore/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainWebMobiView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlAddress = "http://www.google.com"
+        let url = NSURL(string: urlAddress)
+        let requestObj = NSURLRequest(URL: url!)
+        self.mainWebMobiView.loadRequest(requestObj)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
